5181cd7e25799c654a8104f9e867950dd1bbe4b2
Roshan <thaparoshan3722@gmail.com>
2022-12-19 20:52:32 +0545

    little change ub footer and mode

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+const mainBody=document.querySelector("#Main-Container");
+
+const modeChangeButton=document.querySelector("#Mode-Change");
+
+let isDark=true;
+
+// Added Events
+modeChangeButton.addEventListener('click',ModeChange);
+
+
+function ModeChange()
+{
+    if(isDark)
+    {
+        mainBody.className="Light-Mode";
+        modeChangeButton.innerHTML="D";
+        modeChangeButton.className="Light-Mode"
+    }
+    else
+    {
+        mainBody.className="Dark-Mode";
+        modeChangeButton.innerHTML="L";
+        modeChangeButton.className="Dark-Mode"
+    }
+    isDark=!isDark;
+}
